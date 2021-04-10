d95c865a0358819a9d7777e6ba25f4b5d6950d21
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-04-10 18:13:50 +0430

    social comp done

--- client/src/components/Social.js
+++ client/src/components/Social.js
@@ -1,9 +1,27 @@
 import React from "react";
+import HoverImage from "react-hover-image";
+import whatsappImg from "../assets/social/whatsapp.png";
+import whatsappColorImg from "../assets/social/whatsapp-color.png";
+import tgImg from "../assets/social/tg.png";
+import tgColorImg from "../assets/social/tg-color.png";
+import githubImg from "../assets/social/github.png";
+import githubColorImg from "../assets/social/github-color.png";
+import igImg from "../assets/social/ig.png";
+import igColorImg from "../assets/social/ig-color.png";
+import twitterImg from "../assets/social/twitter.png";
+import twitterColorImg from "../assets/social/twitter-color.png";
 
 export default function Social() {
   return (
     <div className="social-wrapper">
-      <p>insta</p>
+      <p>شبکه های اجتماعی</p>
+      <div className="social-icons">
+        <HoverImage src={whatsappImg} hoverSrc={whatsappColorImg} />
+        <HoverImage src={tgImg} hoverSrc={tgColorImg} />
+        <HoverImage src={igImg} hoverSrc={igColorImg} />
+        <HoverImage src={twitterImg} hoverSrc={twitterColorImg} />
+        <HoverImage src={githubImg} hoverSrc={githubColorImg} />
+      </div>
     </div>
   );
 }
