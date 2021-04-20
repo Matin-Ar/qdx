6a9aa2ffa78638a79b11c5bcc06b13e0ccc7e601
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-04-20 16:08:14 +0430

    starting skeleton

--- client/src/components/AllCategoriesPage.js
+++ client/src/components/AllCategoriesPage.js
@@ -1,28 +1,108 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Skeleton from "./skeleton/Skeleton";
+import frontEndIMG from "../assets/courses/frontend.jpg";
 
 export default function AllCategoriesPage() {
-  const [categories, setCategories] = useState([]);
+  const [tutorials, setTutorials] = useState([]);
 
   useEffect(() => {
     axios
-      .get("/categories")
+      .get("/tutorials")
       .then((res) => {
         if ((res.status = 200)) {
-          setCategories(res.data);
-          console.log("categories array is", categories);
+          setTutorials(res.data);
+          console.log("categories array is", tutorials);
         }
       })
       .catch((err) => console.log(err));
-  }, [categories.length]);
+  }, [tutorials.length]);
 
   return (
     <div className="allCategoriesPage-wrapper">
-      <div className="favcategory-container">
-        {categories &&
-          categories.map((category) => {
+      <div className="allCategoriesPage-container">
+        {tutorials.length === 0 && (
+          <React.Fragment>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+            <div className="single-category-wrapper">
+              <Skeleton type="text" />
+
+              <div className="single-category-img-container">
+                <Skeleton type="thumbnail" />
+              </div>
+            </div>
+          </React.Fragment>
+        )}
+
+        <div className="single-category-wrapper">
+          "Front-end"
+          <div className="single-category-img-container">
+            {" "}
+            <img className="single-category-image" src={frontEndIMG}></img>
+          </div>
+        </div>
+
+        {tutorials &&
+          tutorials.map((tutorial) => {
             return (
-              <div className="single-category-container">{category.name}</div>
+              <div className="single-category-wrapper">
+                {tutorial.name}{" "}
+                <div className="single-category-img-container">
+                  {" "}
+                  <img
+                    className="single-category-image"
+                    src={`http://localhost:3001/tutorials/${tutorial.name}/avatar`}
+                  ></img>
+                </div>
+              </div>
             );
           })}
       </div>
--- client/src/components/SingleCoursePage.js
+++ client/src/components/SingleCoursePage.js
@@ -1,14 +1,7 @@
 import React, { Component } from "react";
 
-import dummyCourseImg from "../assets/courses/courseimage/lodash.png";
 import Social from "./Social";
-import SingleCourseTab from "./SingleCourseTab";
-
-import durationICON from "../assets/courses/duration.png";
-import authorICON from "../assets/courses/author.png";
-import languageICON from "../assets/courses/language.png";
-import lessonsICON from "../assets/courses/lessons.png";
-import linkICON from "../assets/courses/link.png";
+import SingleCourseComponent from "./SingleCourseComponent";
 
 export default class SingleCoursePage extends Component {
   constructor(props) {
@@ -18,90 +11,7 @@ export default class SingleCoursePage extends Component {
   render() {
     return (
       <div className="singleCousePage-wrapper">
-        <div className="singleCourse-container">
-          <div className="imageContainer">
-            <img src={dummyCourseImg} />
-          </div>
-          <div className="courseShortDescription-container">
-            <h1>Course Title</h1>
-            <p>
-              Subnautica Below Zero یک بازی جهان باز ( Open World ) و تک نفره می
-              باشد که توسط استودیو Unknown Worlds Entertainment ساخته و در تاریخ
-              30 ژانویه 2019 منتشر شد . شما در یک اقیانوس فضایی هستید که می
-              توانید مناطق مختلف بازی در زیر دریا و روی خشکی را جستجو کنید .
-              Below Zero
-            </p>
-            <div className="infoIcon-container">
-              <div className="infoIcon-top">
-                <div className="singlecourse-description-bottom-icons">
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={durationICON}></img>
-                    <p>03:54:21</p>
-                  </div>
-                  <div className="singlecourse-description-top-icon">
-                    <img src={lessonsICON}></img>
-                    <p>42</p>
-                  </div>
-                  <div className="singlecourse-description-top-icon">
-                    <img src={languageICON}></img>
-                    <p>انگلیسی </p>
-                  </div>
-                  <div className="singlecourse-description-top-icon">
-                    <img src={linkICON}></img>
-                    <p>Udemy.com</p>
-                  </div>
-                  <div className="singlecourse-description-top-icon">
-                    <img src={authorICON}></img>
-                    <p>Andrew Mead</p>
-                  </div>
-                </div>
-              </div>
-              <div className="infoIcon-bottom">
-                <div className="singlecourse-description-bottom-icons">
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={durationICON}></img>
-                    <p>03:54:21</p>
-                  </div>
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={lessonsICON}></img>
-                    <p>42</p>
-                  </div>
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={languageICON}></img>
-                    <p>انگلیسی </p>
-                  </div>
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={linkICON}></img>
-                    <p>Udemy.com</p>
-                  </div>
-                  <div className="singlecourse-description-bottom-icon">
-                    <img src={authorICON}></img>
-                    <p>Andrew Mead</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="courseLongDescription-container">
-            <p>
-              لورم ایپسوم یا طرح‌نما (به انگلیسی: Lorem ipsum) به متنی آزمایشی و
-              بی‌معنی در صنعت چاپ، صفحه‌آرایی و طراحی گرافیک گفته می‌شود. طراح
-              گرافیک از این متن به عنوان عنصری از ترکیب بندی برای پر کردن صفحه و
-              ارایه اولیه شکل ظاهری و کلی طرح سفارش گرفته شده استفاده می نماید،
-              تا از نظر گرافیکی نشانگر چگونگی نوع و اندازه فونت و ظاهر متن باشد.
-              معمولا طراحان گرافیک برای صفحه‌آرایی، نخست از متن‌های آزمایشی و
-              بی‌معنی استفاده می‌کنند تا صرفا به مشتری یا صاحب کار خود نشان دهند
-              که صفحه طراحی یا صفحه بندی شده بعد از اینکه متن در آن قرار گیرد
-              چگونه به نظر می‌رسد و قلم‌ها و اندازه‌بندی‌ها چگونه در نظر گرفته
-              شده‌است. از آنجایی که طراحان عموما نویسنده متن نیستند و وظیفه
-              رعایت حق تکثیر متون را ندارند و در همان حال کار آنها به نوعی
-              وابسته به متن می‌باشد آنها با استفاده از محتویات ساختگی، صفحه
-              گرافیکی خود را صفحه‌آرایی می‌کنند تا مرحله طراحی و صفحه‌بندی را به
-              پایان برند.
-            </p>
-          </div>
-          <SingleCourseTab className="SingleCourseTab" />
-        </div>
+        <SingleCourseComponent />
         <Social />
       </div>
     );
