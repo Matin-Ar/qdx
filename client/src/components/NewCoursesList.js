d41444ab4d8aa041292cf1f812a64a2634c0d51b
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-05-03 20:58:57 +0430

    fixed new course

--- client/src/components/NewCoursesList.js
+++ client/src/components/NewCoursesList.js
@@ -1,14 +1,35 @@
 import React from "react";
+import { useQuery } from "react-query";
 import CourseComponent from "./CourseComponent";
+import axios from "axios";
+import Loader from "./Loader";
 
 export default function NewCoursesList() {
+  const courses = useQuery("newCourses", () => {
+    return axios.get("/courses?limit=5&skip=0").then((res) => {
+      console.log(res.data);
+      return res.data;
+    });
+  });
+
   return (
     <div className="newcourseslist-container">
       <h2>جدید ترین دوره ها</h2>
-      <CourseComponent />
-      <CourseComponent />
-      <CourseComponent />
-      <CourseComponent />
+      {courses.isLoading ? (
+        <div className="all_categories_page_container">
+          <div className="loader-container">
+            <Loader />
+          </div>
+        </div>
+      ) : (
+        <div className="newcourse-wrapper">
+          {courses.data?.map((course) => {
+            return (
+              <CourseComponent course={course} tutorialName={course.title} />
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
