a663d4dedfc189cf64b24a8520b64f954a3783d3
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-05-12 15:30:07 +0430

    bp

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,14 +1,13 @@
 import React from "react";
 import searchicon from "../assets/search-icon.png";
-
+import CourseSeachComponent from "./CourseSeachComponent";
 export default function Hero() {
   return (
     <div className="hero-container">
       <h1 className="hero-title"> دوره های آموزشی برنامه نویسی</h1>
       <p className="hero-subtitle">مهارت محور و مخصوص ورود به بازار کار</p>
       <div className="hero-input-container">
-        <input className="hero-input" type="text" />
-        <img className="hero-input-icon" src={searchicon} autoFocus></img>
+        <CourseSeachComponent />
       </div>
     </div>
   );
