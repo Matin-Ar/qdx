ed57940a3f4ac1569cc6ab931914bd0fec03fdc2
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-05-12 16:45:58 +0430

    bp

--- client/src/components/CourseSeachComponent.js
+++ client/src/components/CourseSeachComponent.js
@@ -1,32 +1,49 @@
-import axios from "axios";
+import axios, { CancelToken } from "axios";
 import React, { useEffect, useState } from "react";
 import searchicon from "../assets/search-icon.png";
+import useQuery from "react-query";
 
 function CourseSeachComponent() {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("salam");
 
-  const source = axios.CancelToken;
+  const query = useQuery("todos", () => {
+    // Create a new CancelToken source for this request
+    const source = CancelToken.source();
 
-  useEffect(() => {
-    console.log(searchValue);
-    axios
-      .get(
-        `/search`,
-        { name: searchValue },
-        { cancelToken: source.CancelToken }
-      )
-      .then((res) => console.log("search result", res.data));
-  }, [searchValue]);
+    const promise = axios.get(`/search/${searchValue}`, {
+      // Pass the source token to your request
+      cancelToken: source.token,
+    });
+
+    // Cancel the request if React Query calls the `promise.cancel` method
+    promise.cancel = () => {
+      source.cancel("Query was cancelled by React Query");
+    };
+
+    return promise;
+  });
+
+  // useEffect(async () => {
+  //   try {
+  //     console.log(searchValue);
+  //     const res = await axios
+  //       .get("/search", {
+  //         data: { name: searchValue },
+  //       })
+  //       .then((res) => console.log("search result", res.data));
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }, [searchValue]);
 
   return (
     <>
-      {searchValue && <h1>{searchValue}</h1>}
       <input
         className="hero-input"
         type="text"
         placeholder="جستجو "
         value={searchValue}
-        onChange={(e) => setSearchValue(e.value)}
+        onChange={(e) => setSearchValue(e.target.value)}
       />
       <img className="hero-input-icon" src={searchicon} autoFocus></img>
     </>
