a1fdb87c950eba2449f4dfe743ca225475e793dd
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-05-12 16:55:48 +0430

    fix confilict2

--- src/routers/test.js
+++ src/routers/test.js
@@ -1,46 +1,38 @@
-const express = require("express");
-const Category = require("../models/category");
-const Tutorial = require("../models/tutorial");
-const Course = require("../models/course");
-const router = new express.Router();
+const express = require('express')
+const Category = require('../models/category')
+const Tutorial = require('../models/tutorial')
+const Course = require('../models/course')
+const router = new express.Router()
 
-router.get("/test", async (req, res) => {
-  try {
-    const category = await Category.find({}, null, { sort: { name: 1 } });
-    const tutorial = await Tutorial.find({}, null, { sort: { name: 1 } });
+router.get('/test', async (req,res) => {    
+    try {
+        const category = await Category.find({ }, null, { sort: { name: 1 } })
+        const tutorial = await Tutorial.find({ }, null, { sort: { name: 1 } })
 
-    let test = [];
-    category.forEach((cat) => {
-      const myObj = {
-        category: cat.name,
-        tutorials: tutorial.filter((tut) => {
-          return tut.cat == cat._id.toString();
-        }),
-      };
-      test.push(myObj);
-    });
-    res.send(test);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+        let test = []
+        category.forEach((cat) => {
+            const myObj = {
+                category: cat.name,
+                tutorials: tutorial.filter((tut) => {
+                    return tut.cat == cat._id.toString()
+                })
+            } 
+            test.push(myObj)
+        })
+        res.send(test)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
 
-router.get("/search/:word", async (req, res) => {
-  console.log(req.params.word);
-  try {
-    const course = await Course.find(
-      { title: { $regex: req.params.word } },
-      "title",
-      {
-        sort: { title: 1 },
-        limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip),
-      }
-    );
-    res.send(course);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+router.get('/search/:word', async (req, res) => {
+    console.log(req.params.word)
+    try {
+        const course = await Course.find({ title: { '$regex': req.params.word } }, 'title', { sort: { title : 1 }, limit: parseInt(req.query.limit), skip: parseInt(req.query.skip) })
+        res.send(course)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
 
-module.exports = router;
+module.exports = router
