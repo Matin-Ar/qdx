0d5200b94d2065c08c99fd06b5ea7a153bd8903d
Alisa-khodadadi <61903569+Alisa-khodadadi@users.noreply.github.com>
2021-05-13 11:13:15 +0430

    fixed search

--- client/src/components/FavCategory.js
+++ client/src/components/FavCategory.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import angularICON from "../assets/categories/angular.png";
 import cSharpICON from "../assets/categories/c-sharp.png";
 import javaICON from "../assets/categories/java.png";
@@ -13,42 +14,59 @@ export default function FavCategory() {
     <div className="favcategory-container">
       <h1 className="favcategory-title">محبوب ترین دسته بندی ها</h1>
       <div className="category-wrapper">
-        <div className="single-category-container">
-          <img src={javascriptICON}></img>
-          <p>JAVASCRIPT</p>
-        </div>
-        <div className="single-category-container">
-          <img src={reactjsICON}></img>
-          <p>ReactJS</p>
-        </div>
-        <div className="single-category-container">
-          <img src={pythonICON}></img>
-          <p>PYTHON</p>
-        </div>
+        <Link to="/categories/javascript">
+          <div className="single-category-container">
+            <img src={javascriptICON}></img>
+            <p>JAVASCRIPT</p>
+          </div>
+        </Link>
+
+        <Link>
+          <div className="single-category-container">
+            <img src={reactjsICON}></img>
+            <p>ReactJS</p>
+          </div>
+        </Link>
+
+        <Link>
+          <div className="single-category-container">
+            <img src={pythonICON}></img>
+            <p>PYTHON</p>
+          </div>
+        </Link>
+
         <div className="single-category-container">
           <img src={javaICON}></img>
           <p>JAVA</p>
         </div>
+
         <div className="single-category-container">
           <img src={cSharpICON}></img>
           <p>C-Sharp</p>
         </div>
+
         <div className="single-category-container">
           <img src={angularICON}></img>
           <p>Angular</p>
         </div>
+
         <div className="single-category-container">
           <img src={typescriptICON}></img>
           <p>TypeScript</p>
         </div>
-        <div className="single-category-container">
-          <img src={nodejsICON}></img>
-          <p>NODE JS</p>
-        </div>
+
+        <Link to="categories/nodejs">
+          <div className="single-category-container">
+            <img src={nodejsICON}></img>
+            <p>NODE JS</p>
+          </div>
+        </Link>
       </div>
-      <button className="default-button favcategory-button">
-        دسته بندی های بیشتر
-      </button>
+      <Link to="/categories">
+        <button className="default-button favcategory-button">
+          دسته بندی های بیشتر
+        </button>
+      </Link>
     </div>
   );
 }
